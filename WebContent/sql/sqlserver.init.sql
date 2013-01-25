6d31f6456b6ae5cd0fabd6a1a64ad39bc3c9cb85
3434 <404755455@qq.com>
2013-01-25 13:59:02 +0800

    update 1.0

--- WebContent/sql/sqlserver.init.sql
+++ WebContent/sql/sqlserver.init.sql
@@ -1,28 +1,25 @@
-if object_id('[Account]') is null create table [Account](
-	accountId int not null identity(10000, 1),
-	nickName varchar(16),
-	pwdMd5 varchar(32),
-	privilege int not null default 0,
-	userId varchar(16) not null unique,
-	scores int not null default 0,
-	goldCoin int not null default 0,
-	gameCoin int not null default 0,
-	createTime datetime not null,
-	updateTime datetime not null,
-	state int not null default 0,
-	primary key(accountId)
+if object_id('[Administrator]') is null create table [Administrator](
+	id int not null identity(0, 1),
+	name varchar(16),
+	passWord varchar(16),
+	role varchar(20) not null,
+	authority varchar(20) not null,
+	createtime datetime ,
+	logintime datetime 
+	primary key(id)
 );
 	
-if object_id('[AccountPermission]') is null create table [AccountPermission](
-	accountId int not null,
-	daySubscribeLimit int not null default 0,
-	monthSubscribeLimit int not null default 0,
-	lastSubscribeTime datetime,
-	daySubscribeAmount int not null default 0,
-	monthSubscribeAmount int not null default 0,
-	totalSubscribeAmount int not null default 0,
-	accessPermission int not null default 0,
-	subscribePermission int not null default 0,
-	primary key(accountId)
+if object_id('[DataDictionary]') is null create table [DataDictionary](
+	id int not null identity(0, 1),
+	name varchar(16) not null,
+	value varchar(16) not null,
+	primary key(id)
 );
 
+insert into Administrator(name,password,role,authority,createtime,logintime) values('ohyeah','ohyeah','admin','1','2011-12-02','2011-12-02');
+insert into DataDictionary(name,value) values('authority', '1');
+insert into DataDictionary(name,value) values('authority', '2');
+insert into DataDictionary(name,value) values('authority', '3');
+insert into DataDictionary(name,value) values('role', 'admin');
+insert into DataDictionary(name,value) values('role', 'worker');
+insert into DataDictionary(name,value) values('role', 'partner');
